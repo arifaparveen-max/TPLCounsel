ca5522094cb5fe0877cb389930e4db0c7eb672f3
PAkhtars <akhtararifaparveen@gmail.com>
2026-07-09 12:03:30 +0530

    First Initials 3

--- src/app/aboutus/aboutus.ts
+++ src/app/aboutus/aboutus.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-aboutus',
-  imports: [],
+  imports: [Footer],
   templateUrl: './aboutus.html',
   styles: ``,
 })
