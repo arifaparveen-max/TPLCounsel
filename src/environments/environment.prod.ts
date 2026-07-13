dc6ffad868d25e9e4b2b6b84e05be313716b36e2
PAkhtars <akhtararifaparveen@gmail.com>
2026-07-13 11:21:40 +0530

    Live Streaming1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  hubUrl: 'https:/employeesapi.runasp.net/liveStreamHub',  // Update with your production URL
+  hubUrl: 'https://employeesapi.runasp.net/liveStreamHub',  // Update with your production URL
 };
